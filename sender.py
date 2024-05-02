a397367e84281d63220929a431ff00be2ecd779b
“Crystal <K19001568@kcl.ac.uk>
2024-05-03 00:20:31 +0100

    Neaten email format

--- sender.py
+++ sender.py
@@ -36,28 +36,28 @@ class Sender:
     def attachLink(self, property):
         url = property.getUrl()
         link = f"""
-        View <a href="https://{url}">listing</a><br>
+        View <a href="https://{url}">listing</a></p>
         """
         self.msg.attach(MIMEText(link, 'html'))
     
     def attachPrices(self, property):
         price = property.getPricePer()
         total = property.getTotal()
-        prices = f"<p>Price per night: £{price}, Total: £{total}</p><br>"
+        prices = f"<p>Price per night: £{price}, Total: £{total}</p>"
         self.msg.attach(MIMEText(prices, 'html'))
     
     def attachLocation(self, property, distance, name):
+        if distance:
+            self.msg.attach(MIMEText(f"<p>The property is roughly {distance}km from Oktoberfest.</p>", 'html'))
+        else:
+            self.msg.attach(MIMEText("<p>Could not locate this property.</p>", 'html'))
         if name:
             distanceName = property.getDistanceName() 
-            distanceHtml = f"<p>, {distanceName}km from Oktoberfest.<br>" if distanceName else "<br>"
+            distanceHtml = f"<p>, {distanceName}km from Oktoberfest.</p>" if distanceName else "<br>"
             location = f"<p>Based near {name}{distanceHtml}</p>"
             self.msg.attach(MIMEText(location, 'html'))
         else:
-            self.msg.attach(MIMEText("<p>There were no local points of interest.</p><br>", 'html'))
-        if distance:
-            self.msg.attach(MIMEText(f"<p>The property is roughly {distance}km from Oktoberfest.</p><br>", 'html'))
-        else:
-            self.msg.attach(MIMEText("<p>Could not locate this property</p><br>", 'html'))
+            self.msg.attach(MIMEText("<p>There were no local points of interest.</p>", 'html'))
 
 
     def attachCommute(self, property, start):
@@ -68,28 +68,29 @@ class Sender:
                 commutes = property.getCommute()
             else:
                 commutes = property.getCommuteName()
-            self.msg.attach(MIMEText(f"<p>How to get to Oktoberfest from {start}:</p><br>", 'html'))
-            commuteStr = ""
-            for idx in range(len(commutes)):
-                self.msg.attach(MIMEText(f"<h5>Commute option {idx+1}</h5><br>", 'html'))
-                commuteStr += commutes[idx]
-                commuteStr += "<br>"
-                self.msg.attach(MIMEText(commuteStr, 'html'))
+            if commutes:
+                if commutes[0] != "":
+                    self.msg.attach(MIMEText(f"<p>How to get to Oktoberfest from {start}:</p>", 'html'))
+                    commuteStr = ""
+                    for idx in range(len(commutes)):
+                        self.msg.attach(MIMEText(f"<h4>Option {idx+1}</h4>", 'html'))
+                        commuteStr += f"<p>{commutes[idx]}</p>"
+                        self.msg.attach(MIMEText(commuteStr, 'html'))
         else:
-            self.msg.attach(MIMEText("<p>There were no calculated commutes.</p><br>", 'html'))
+            self.msg.attach(MIMEText("<p>There were no calculated commutes.</p>", 'html'))
 
     def writeEmail(self, properties, emailTo):
         self.msg["From"] = "ilikecows3359@gmail.com"
         self.msg["To"] = emailTo
         self.msg["Subject"] = f"Airbnb search {self.getDate()}"
         
-        heading = f"<h6>Sir, we found {len(properties)} listings for you.</h6>"
+        heading = f"<h4>Sir, we found {len(properties)} listing(s) for you.</h4>"
         self.msg.attach(MIMEText(heading, 'html'))
 
         if properties:
             idx = 1
             for property in properties:
-                self.msg.attach(MIMEText(f"<p><b>{idx}.</b></p>", 'html'))
+                self.msg.attach(MIMEText(f"<p><b>{idx}.</b>", 'html'))
                 self.attachLink(property)
                 self.attachImage(property)
                 self.attachPrices(property)
